b1ff1df83bc654a18fb9c727b260f291c160932e
talhaaydin <mt.aydin@outlook.com>
2025-03-17 23:08:04 +0300

    vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   build: {
     emptyOutDir: true,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr(), // Add the SVGR plugin here
+  ],
 });
